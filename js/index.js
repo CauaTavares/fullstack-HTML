5a79f34c4ab848371248dfe2ac933a929b38847a
CauaTavares <cauaxd2010@outlook.com>
2023-02-07 22:22:30 -0300

    Comecei JS

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// let nome = prompt ("O primeiro nome")
+// let sobrenome = prompt ("O sobrenome")
+// let campo = prompt ("O campo de estudo")
+// let nascimento = prompt ("O ano de nascimento")
+
+// alert(nome + " " + sobrenome + " " + campo + " " + nascimento)
+
+
+let num1 = Number(prompt("primeiro valor"))
+let num2 = Number(prompt("segundo valor"))
+
+alert (num1 + num2)
+alert (num1 - num2)
+alert (num1 * num2)
+alert (num1 / num2)
+
+console.log();
