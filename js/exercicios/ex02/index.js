45c2a916c5a6198466e9901000ce4d1128c66f38
CauaTavares <cauaxd2010@outlook.com>
2023-02-09 20:41:24 -0300

    Finalizei a aula 1

--- js/exercicios/ex02/index.js
+++ js/exercicios/ex02/index.js
@@ -1,28 +1,30 @@
-// const valor1 = prompt("Primeiro valor") 
-// const valor2 = prompt("Segundo valor") 
+const primeiroV = prompt("Primeiro valor") 
+const segundoV = prompt("Segundo valor") 
+
+const valor1 = parseFloat(primeiroV)
+const valor2 = parseFloat(segundoV)
+
+window.alert(valor1 + valor2)
+window.alert(valor1 - valor2)
+window.alert(valor1 * valor2)
+window.alert(valor1 / valor2)
 
 
-// window.alert(valor1 + valor2)
-// window.alert(valor1- valor2)
-// window.alert(valor1 * valor2)
-// window.alert(valor1 / valor2)
+// const entrada1 = prompt("Informe o primeiro numero:")
+// const entrada2 = prompt("Informe o segundo numero:")
 
+// const x = parseFloat(entrada1)
+// const y = parseFloat(entrada2) 
 
-const entrada1 = prompt("Informe o primeiro numero:")
-const entrada2 = prompt("Informe o segundo numero:")
+// const soma = x + y
+// const subtracao = x - y
+// const multiplicacao = x * y
+// const divisao = x / y
 
-const x = parseFloat(entrada1)
-const y = parseFloat(entrada2) 
-
-const soma = x + y
-const subtracao = x - y
-const multiplicacao = x * y
-const divisao = x / y
-
-alert(
-    "Resultados:\n" + 
-    "\nSoma: " + soma + 
-    "\nSubtração: " + subtracao + 
-    "\nMultiplicação: " + multiplicacao + 
-    "\nDivisão: " + divisao 
-)
+// alert(
+//     "Resultados:\n" + 
+//     "\nSoma: " + soma + 
+//     "\nSubtração: " + subtracao + 
+//     "\nMultiplicação: " + multiplicacao + 
+//     "\nDivisão: " + divisao 
+// )
