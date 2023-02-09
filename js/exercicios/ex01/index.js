32e0ef0ee074d7490d5b946f6804c60c7a220d04
CauaTavares <cauaxd2010@outlook.com>
2023-02-08 23:45:44 -0300

    Correção.

---
+++ js/exercicios/ex01/index.js
@@ -0,0 +1,7 @@
+const nome = prompt("Primeiro nome")
+const sobrenome = prompt("Segundo nome")
+const escola = prompt("O campo de estudo")
+const nasc = prompt("O ano de nascimento")
+
+
+window.alert (nome + " " + sobrenome + " " + escola + " " + nasc)
---
+++ js/exercicios/ex03/index.js
@@ -0,0 +1,10 @@
+// const nome1 = prompt ("nome do primeiro veiculo")
+const veiculo1 = prompt ("velocidade do primeiro veiculo")
+// const nome2 = prompt ("nome do segundo veiculo")
+const veiculo2 = prompt ("velocidade do segundo veiculo")
+
+if (veiculo1 > veiculo2) {
+    alert(veiculo1)
+} else {
+    alert(veiculo2)
+}
