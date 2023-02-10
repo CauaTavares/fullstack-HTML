6379f4b8a53b195f53496878acaeb93270b63850
CauaTavares <cauaxd2010@outlook.com>
2023-02-10 19:56:31 -0300

    Comecei o ex04

--- js/exercicios/ex02/index.js
+++ js/exercicios/ex02/index.js
@@ -1,30 +1,31 @@
-const primeiroV = prompt("Primeiro valor") 
-const segundoV = prompt("Segundo valor") 
+// const primeiroV = prompt("Primeiro valor") 
+// const segundoV = prompt("Segundo valor") 
 
-const valor1 = parseFloat(primeiroV)
-const valor2 = parseFloat(segundoV)
+// const valor1 = parseFloat(primeiroV)
+// const valor2 = parseFloat(segundoV)
 
-window.alert(valor1 + valor2)
-window.alert(valor1 - valor2)
-window.alert(valor1 * valor2)
-window.alert(valor1 / valor2)
+// window.alert(valor1 + valor2)
+// window.alert(valor1 - valor2)
+// window.alert(valor1 * valor2)
+// window.alert(valor1 / valor2)
 
 
-// const entrada1 = prompt("Informe o primeiro numero:")
-// const entrada2 = prompt("Informe o segundo numero:")
+const entrada1 = prompt("Informe o primeiro numero:")
+const entrada2 = prompt("Informe o segundo numero:")
 
-// const x = parseFloat(entrada1)
-// const y = parseFloat(entrada2) 
+const x = parseFloat(entrada1)
+const y = parseFloat(entrada2) 
 
-// const soma = x + y
-// const subtracao = x - y
-// const multiplicacao = x * y
-// const divisao = x / y
+const soma = x + y
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y
 
-// alert(
-//     "Resultados:\n" + 
-//     "\nSoma: " + soma + 
-//     "\nSubtração: " + subtracao + 
-//     "\nMultiplicação: " + multiplicacao + 
-//     "\nDivisão: " + divisao 
-// )
+alert(
+    "Resultados:\n" + 
+    "\nSoma: " + soma + 
+    "\nSubtração: " + subtracao + 
+    "\nMultiplicação: " + multiplicacao + 
+    "\nDivisão: " + divisao 
+)
+
--- js/exercicios/ex04/index.js
+++ js/exercicios/ex04/index.js
@@ -1,1 +1,16 @@
-const 
+// const nomePoder = prompt("Insira o nome do poder")
+const forca = prompt("Insira a força do poder")
+// const nomePersonagem = prompt("Insira o nome do personagem")
+// const vida = prompt("Pontos de vida")
+const defesa = prompt("Insira o poder de defesa do oponente")
+const escudo = confirm("O oponente tem vai ter escudo?")
+
+
+
+if (forca > defesa) {
+    alert("O dano aplicado sera de " + (forca - defesa))
+} else if(forca > defesa && escudo) {
+    alert("O dano aplicado é de " + ((forca - defesa) / escudo))
+}
+
+
