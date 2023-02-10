9f7d2a52b9da6f69947a5f7edf6be98d8c1fa29f
CauaTavares <cauaxd2010@outlook.com>
2023-02-09 21:26:13 -0300

    Finalizei o ex04

--- js/exercicios/ex03/index.js
+++ js/exercicios/ex03/index.js
@@ -1,10 +1,13 @@
-// const nome1 = prompt ("nome do primeiro veiculo")
+const nome1 = prompt ("nome do primeiro veiculo")
 const veiculo1 = prompt ("velocidade do primeiro veiculo")
-// const nome2 = prompt ("nome do segundo veiculo")
+const nome2 = prompt ("nome do segundo veiculo")
 const veiculo2 = prompt ("velocidade do segundo veiculo")
 
 if (veiculo1 > veiculo2) {
-    alert(veiculo1)
+    alert("O " + nome1 + " esta mais rapido na velocidade " + veiculo1)
+} else if(veiculo2 > veiculo1) {
+    alert("O veiculo 2 esta mais rapido a " + veiculo2)
 } else {
-    alert(veiculo2)
+    alert("Os veiculos entâo na mesma velocidade")
 }
+
