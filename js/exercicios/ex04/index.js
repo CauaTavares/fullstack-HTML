33ff4864d033885a4d406885ed7293ba931ef45f
CauaTavares <cauaxd2010@outlook.com>
2023-02-11 19:28:10 -0300

    FINALIZEI O EX04

--- js/exercicios/ex04/index.js
+++ js/exercicios/ex04/index.js
@@ -1,16 +1,24 @@
-// const nomePoder = prompt("Insira o nome do poder")
+const name1 = prompt("Insira o nome do Herói")
 const forca = prompt("Insira a força do poder")
-// const nomePersonagem = prompt("Insira o nome do personagem")
-// const vida = prompt("Pontos de vida")
+
+const name2 = prompt("Insira o nome do vilão")
 const defesa = prompt("Insira o poder de defesa do oponente")
 const escudo = confirm("O oponente tem vai ter escudo?")
+let vida = prompt("Pontos de vida")
+
+let dano = 0
 
 
-
-if (forca > defesa) {
-    alert("O dano aplicado sera de " + (forca - defesa))
-} else if(forca > defesa && escudo) {
-    alert("O dano aplicado é de " + ((forca - defesa) / escudo))
+if (forca > defesa && escudo == false) {
+    dano = forca - defesa
+    alert("O dano aplicado sera de " + dano)
+} else if(forca > defesa && escudo == true) {
+    dano = ((forca - defesa) / 2)
+    alert("O dano aplicado é de " + dano)
+} else if(forca < defesa) {
+    alert("O dano causado foi de: 00")
 }
 
+vida = vida - dano
 
+alert(name1 + "\nFORÇA: " + forca + "\n\n" + name2 + "\nVida: " + vida + "\nDefesa: " + defesa)
