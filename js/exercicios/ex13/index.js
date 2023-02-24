91a736380c377d9ecd0e39701b7923f7db3db769
CauaTavares <cauaxd2010@outlook.com>
2023-02-24 11:11:25 -0300

    Finalizei o ex13

--- js/exercicios/ex13/index.js
+++ js/exercicios/ex13/index.js
@@ -1,34 +1,46 @@
-let arr = []
+let arrs = []
 let opcao = ""
 
-
  do {
-    opcao = prompt("Imoveis cadastrados: " + arr.length + "\n" + "Escolha uma opção:" + "\n1) Salvar um imovel" + "\n2) Mostrar todos" + "\n3) Sair")
-
-    if (arr.length > 0) {
-        opcao
-    }
+    opcao = prompt("Imoveis cadastrados: " + arrs.length + 
+    "\n" + "Escolha uma opção:" + 
+    "\n1) Salvar um imovel" + 
+    "\n2) Mostrar todos" + 
+    "\n3) Sair"
+    )
 
     if (opcao == 1) {
-        let pessoa = {}
-        let nome = prompt("Nome do proprietario:")
-        let quartos = prompt("Quantidade de quarto/s:")
-        let banheiro = prompt("Quantidade de banheiro/s:")
-        let garagem = prompt("Quantidade de garagem/ns:")
+        let arr = {}
+        arr.proprietario = prompt("Nome do proprietario:")
+        arr.quartos = prompt("Quantidade de quarto(s):")
+        arr.banheiro = prompt("Quantidade de banheiro(s):")
+        arr.garagem = prompt("Quantidade de garagem(ns):")
 
-        pessoa.nome = nome
-        pessoa.quartos = quartos
-        pessoa.banheiro = banheiro
-        pessoa.garagem = garagem
-        arr.push(pessoa)
-        alert("Você adicionou o imovel: " + "\nNOME: " + nome + "\nQUARTOS: " + quartos + "\nBANHEIROS: " + banheiro + "\nGARAGEM: "+ garagem)
+        const confirmacao = confirm("Gostaria de salvar o imovel? " + 
+        "\nNOME: " + arr.proprietario + 
+        "\nQUARTOS: " + arr.quartos + 
+        "\nBANHEIROS: " + arr.banheiro + 
+        "\nGARAGEM: "+ arr.garagem
+        )
+
+        if (confirmacao) {
+            arrs.push(arr)
+            alert("Imóvel salvo com sucesso!")
+        }
 
     } else if (opcao == 2) {
-        
+        for(let i = 0; i < arrs.length; i++) {
+            alert(
+                "Imóvel " + (i + 1) +
+                "\nProprietário: " + arrs[i].proprietario +
+                "\nQuartos: " + arrs[i].quartos +
+                "\nBanheiros: " + arrs[i].banheiro + 
+                "\nGaragem: " + arrs[i].garagem
+            )
+        }
     }
 
 } while (opcao !== "3")
 
-
-alert("FECHOU..........")
+alert("PROGRAMA ENCERRADO.")
 
