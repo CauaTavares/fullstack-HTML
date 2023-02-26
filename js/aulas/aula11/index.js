6e445bd4a5bff7d86d934e380aaae56085095855
CauaTavares <cauaxd2010@outlook.com>
2023-02-26 11:58:52 -0300

    Finalizei a aula 11

--- js/aulas/aula11/index.js
+++ js/aulas/aula11/index.js
@@ -7,8 +7,9 @@ let pessoa = {
         console.log("Olá, mundo! Meu nome é " + this.nome);
     }
 }
-// console.log(pessoa);
-// pessoa.dizerOla()
+console.log(pessoa);
+pessoa.dizerOla()
+
 
 // FUNÇÕES RECURSIVAS
 
@@ -20,14 +21,42 @@ function dividir(num) {
         return num
     }
 }
+dividir(256)
 
-// dividir(256)
 
 function dobrar(num) {
     console.log(num);
     dobrar(num * 2)
 }
-
 dobrar(9)
 
 
+function fatorial(num) {
+    console.log("Numero: " + num);
+    if (num === 0) {
+        return 1
+    } else if (num === 1) {
+        return 1
+    } else {
+        console.log(num + " * !" + (num - 1) );
+        return num * fatorial(num - 1)
+    }
+}
+console.log(fatorial(5));
+
+
+// FUNÇÕES ANÔNIMAS
+
+function somar(a, b) {
+    return a + b
+}
+
+let operacao = somar
+
+console.log(operacao(4, 5));
+
+operacao = subtrair = function (a, b) {
+    return a - b
+}
+
+console.log(subtrair(36, 13));
