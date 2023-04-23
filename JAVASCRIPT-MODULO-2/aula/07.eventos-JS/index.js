f8ec779e74b4607faca4c97b40f70c6e1358b6d3
CauaTavares <cauaxd2010@outlook.com>
2023-04-23 00:41:21 -0300

    finalizei a aula 07

--- JAVASCRIPT-MODULO-2/aula/07.eventos-JS/index.js
+++ JAVASCRIPT-MODULO-2/aula/07.eventos-JS/index.js
@@ -1,12 +1,27 @@
 function register(ev) {
     console.log(ev);
-    const username = ev.currentTarget.children.username.value
-    const password = ev.currentTarget.children.password.value
-    const passwordConfirmation = ev.currentTarget.children.passwordConfirmation.value
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation =sectionElement.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation) {
+        alert('Usuario ' + username + ' registrado')
+    } else {
+        alert('As senhas não conferem!')
+    }
 }
-
-
 
 const button = document.getElementById('register-button')
 
 button.addEventListener('click', register)
+
+function removeListener() {
+    button.removeEventListener('click', register)
+    alert('Event removed')
+}
+
+button.addEventListener('mouseover', function (ev) {
+    console.log(ev.currentTarget);
+})
+
