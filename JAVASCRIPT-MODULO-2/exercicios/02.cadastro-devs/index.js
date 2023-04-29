189f644e76b040b8eecbf2a9e0170358121b80c7
CauaTavares <cauaxd2010@outlook.com>
2023-04-29 18:16:39 -0300

    update

--- JAVASCRIPT-MODULO-2/exercicios/02.cadastro-devs/index.js
+++ JAVASCRIPT-MODULO-2/exercicios/02.cadastro-devs/index.js
@@ -1,31 +1,43 @@
-function createlabel(text, htmlFor) {
-    const  label = document.createElement('label')
+function createLabel(text, htmlFor) {
+    const label = document.createElement('label')
     label.htmlFor = htmlFor
     label.innerText = text
     return label
-}
-
-function createInput(id, value, name, type = 'text', placeholder = '') {
+  }
+  
+  function createInput(id, value, name, type = 'text', placeholder = '') {
     const input = document.createElement('input')
-    input.id = id 
+    input.id = id
     input.value = value
-    input.name = name 
+    input.name = name
     input.type = type
     input.placeholder = placeholder
-    return input 
-}
-
-const addTechBtn = document.getElementById('addtTechBtn')
-const form = document.getElementById('devForm')
-const developers = []
-let inputRows = 0
-
-addTechBtn.addEventListener('click', function (ev) {
-    const stackInput = document.getElementById('StackInputs')
-
-    const newRow = document.createElement('li')
-    const rowIndex = inputRows
+    return input
+  }
     
+    const addTechBtn = document.getElementById('addTechBtn')
+    const form = document.getElementById('devForm')
+    const developers = []
+    let inputRows = 0
+
+
+    addTechBtn.addEventListener('click', function (ev) {
+        const stackInputs = document.getElementById('stackInputs')
+      
+        const newRow = document.createElement('li')
+        const rowIndex = inputRows
+        inputRows++
+        newRow.id = 'inputRow-' + rowIndex
+        newRow.className = 'inputRow'
+      
+        const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
+        const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
+    newRow.append(
+        techNameLabel, techNameInput
+    )
+
+    stackInputs.appendChild(newRow)
+
 })
 
 
