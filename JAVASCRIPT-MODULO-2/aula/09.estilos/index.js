53c896bba570e6fcb47081e06252550f2a767d14
CauaTavares <cauaxd2010@outlook.com>
2023-04-30 00:06:13 -0300

    update

---
+++ JAVASCRIPT-MODULO-2/aula/09.estilos/index.js
@@ -0,0 +1,6 @@
+function useLightTheme () {
+    document.body.style.color = '#212529'
+    document.body.style.backgroundColor = '#f1f5f9'
+}
+
+document.getElementById('lightBtn').addEventListener('click', useLightTheme)
